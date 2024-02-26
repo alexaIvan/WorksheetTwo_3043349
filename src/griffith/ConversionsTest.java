34a26779f7ae9cb36b46776e048f9c00075c6f3e
Alexandra Ivan <alexandra.ivan333@gmail.com>
2024-02-26 23:18:58 +0000

    first junit test implementation of celsius to fahrenheit

--- src/griffith/ConversionsTest.java
+++ src/griffith/ConversionsTest.java
@@ -1,4 +1,5 @@
 package griffith;
+
 /*Student name: Alexandra Ivan
  * Student number: 3043349
  */
@@ -10,29 +11,38 @@ class ConversionsTest {
 
 	@Test
 	void celsiusToFahrenheitConversionTest1() {
-		fail("Not yet implemented");
+		// declare an object of Conversions class and name it conversions
+		Conversions conversions = new Conversions();
+		// declare double variable called celsius
+		double celsius = 32;
+		// declare the actual variable that needs to be compared by calling the method
+		double actual = conversions.celsiusToFahrenheit(celsius);
+		// declacre the actual value of the variable that needs to be compared
+		double expected = 89.6;
+		// call the junit test and compare that the two values are equal
+		assertEquals(expected, actual);
 	}
-	
+
 	@Test
 	void celsiusToFahrenheitConversionTest2() {
 		fail("Not yet implemented");
 	}
-	
+
 	@Test
 	void celsiusToFahrenheitConversionTest3() {
 		fail("Not yet implemented");
 	}
-	
+
 	@Test
 	void fahrenheitToCelsiusConversionTest1() {
 		fail("Not yet implemented");
 	}
-	
+
 	@Test
 	void fahrenheitToCelsiusConversionTest2() {
 		fail("Not yet implemented");
 	}
-	
+
 	@Test
 	void fahrenheitToCelsiusConversionTest3() {
 		fail("Not yet implemented");
@@ -42,57 +52,57 @@ class ConversionsTest {
 	void milesToKilometerConversionTest1() {
 		fail("Not yet implemented");
 	}
-	
+
 	@Test
 	void milesToKilometerConversionTest2() {
 		fail("Not yet implemented");
 	}
-	
+
 	@Test
 	void milesToKilometerConversionTest3() {
 		fail("Not yet implemented");
 	}
-	
+
 	@Test
 	void kilometerToMilesConversionTest1() {
 		fail("Not yet implemented");
 	}
-	
+
 	@Test
 	void kilometerToMilesConversionTest2() {
 		fail("Not yet implemented");
 	}
-	
+
 	@Test
 	void kilometerToMilesConversionTest3() {
 		fail("Not yet implemented");
 	}
-	
+
 	@Test
 	void upperCaseToLowerCaseConversionTest1() {
 		fail("Not yet implemented");
 	}
-	
+
 	@Test
 	void upperCaseToLowerCaseConversionTest2() {
 		fail("Not yet implemented");
 	}
-	
+
 	@Test
 	void upperCaseToLowerCaseConversionTest3() {
 		fail("Not yet implemented");
 	}
-	
+
 	@Test
 	void lowerCaseToUpperCaseConversionTest1() {
 		fail("Not yet implemented");
 	}
-	
+
 	@Test
 	void lowerCaseToUpperCaseConversionTest2() {
 		fail("Not yet implemented");
 	}
-	
+
 	@Test
 	void lowerCaseToUpperCaseConversionTest3() {
 		fail("Not yet implemented");
