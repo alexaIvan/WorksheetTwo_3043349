8ed77f175e68a2ddcca0a78ff4a0d7b5e3109bc3
Alexandra Ivan <alexandra.ivan333@gmail.com>
2024-02-26 19:48:43 +0000

    commit the method stubs for class Conversions

--- src/griffith/Conversions.java
+++ src/griffith/Conversions.java
@@ -1,5 +1,30 @@
 package griffith;
 
 public class Conversions {
+	
+	public double celsiusToFahrenheit(double celsius) {
+		
+	}
+	
+	
+	public double fahrenheitToCelsius(double fahrenheit) {
+	
+	}
+	
+	public double milesToKilometer(double miles) {
+		
+	}
+	
+	public double kilometerToMiles(double kilometer) {
+		
+	}
+	
+	public String upperCaseToLowerCase(String upperCase) {
+		
+	}
+	
+	public String lowerCaseToUpperCase(String lowerCase) {
+		
+	}
 
 }
