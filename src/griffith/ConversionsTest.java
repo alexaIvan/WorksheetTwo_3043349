37251b6843fcda885f2f741358daffa99a4a9677
Alexandra Ivan <alexandra.ivan333@gmail.com>
2024-02-26 23:41:04 +0000

    commit third test for celsius to fahrenheit

--- src/griffith/ConversionsTest.java
+++ src/griffith/ConversionsTest.java
@@ -39,7 +39,18 @@ class ConversionsTest {
 
 	@Test
 	void celsiusToFahrenheitConversionTest3() {
-		fail("Not yet implemented");
+		// declare two object of Conversions class and name it conversions1 and conversions2
+		Conversions conversions1 = new Conversions();
+		Conversions conversions2 = new Conversions();
+		// declare double variable called celsius
+		double celsius1 = 38;
+		// declare double variable called celsius
+		double celsius2 = 37.6;
+		// declare the actual variables that needs to be compared by calling the method 
+        double value1 = conversions1.celsiusToFahrenheit(celsius1);
+        double value2 = conversions2.celsiusToFahrenheit(celsius2);
+        // call junit and assert that the two values are not equal 
+        assertFalse(value1 == value2, "value 1 is equal to value 2");	
 	}
 
 	@Test
